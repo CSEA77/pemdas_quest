81a310873e496d688fe20374dcd1deedda021aee
CSEA77 <110508950+CSEA77@users.noreply.github.com>
2022-08-07 18:51:14 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#Belajar Output
+
+print("Hello World")
+print("Nama Saya Casey")
+print("saya wibu")
+print("overthinking")
+print("ntnt")
+print("at least")
+print("mixue yo")
+print("chou tank item physical")
+print("ambon sasimo")
+print("kudaambon")
---
+++ python2.py
@@ -0,0 +1,14 @@
+# Belajar Input
+
+nama = "Casey" 
+umur = 15
+alamat = "Cimanggis, Kota Depok"
+
+print (nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
+
+print("Nama saya adalah " + nama + " Umur Saya " + str(umur) + " alamat saya " + alamat)
+
