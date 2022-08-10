99c41ce9b69df47a45b98f579930cb7049dd2208
CSEA77 <110508950+CSEA77@users.noreply.github.com>
2022-08-10 01:29:04 -0700

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,10 @@
+#Belajar Input 2
+
+
+nama = input("Masukkan Nama Anda: ")
+nama_depan = input("Masukkan nama depan anda: ")
+print("Nama anda adalah", nama)
+print("Nama depan anda adalah", nama_depan)
+
+
+
---
+++ python4.py
@@ -0,0 +1,29 @@
+#Belajar Input 3
+
+#Belajar Matematika dalam program
+
+
+# a = 5
+# b = 10
+# c = a + b
+
+# print("a:", a)
+# print("b:", b)
+# print("c:", c)
+
+
+# x = int(input("Masukkan Angka ke-1: "))
+# y = int(input("Masukkan Angka ke-2: "))
+
+# print("Nilai X=",x)
+# print("Nilai Y=",y)
+
+# print("X + Y = ", x+y)
+# print("X - Y = ", x-y)
+# print("X * Y = ", x*y)
+# print("X / Y = ", x/y)
+
+
+celcius = int(input("Masukkan Suhu Celcius:"))
+fah = (9/5 * celcius) + 32
+print(celcius, "Celcius =", fah, "Fahrenheit")
